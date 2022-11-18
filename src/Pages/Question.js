266a1b0da39f7b01e081fbad47028b97e5347a3b
Grayson Harvey <graycodes.nu@gmail.com>
2022-11-17 19:45:04 -0600

    added autocomplete dropdown. need to call song data from backend.

--- src/Pages/Question.js
+++ src/Pages/Question.js
@@ -1,12 +1,30 @@
-import { questionData } from "../data/questionData";
+// Data 
+import { useQuery } from "@apollo/client";
+import { QUERY_SONGS } from "../utils/queries";
+
+// MUI
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
 function Question({ question, list }) {
-  // // const { treat } = props
-console.log(question, "question test in question.js")
+  console.log(question, "question test in question.js");
+
   return (
     <>
+      {/* Question */}
       <div key={question.id}>
         <h5>{question.text}</h5>
+      </div>
+      {/* Song Data */}
+      <div>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={""}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} 
+          label="Song" />}
+        />
       </div>
     </>
   );
