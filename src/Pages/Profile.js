59b2c9ffed902410167eb325238b951ff24ed602
Grayson Harvey <graycodes.nu@gmail.com>
2022-11-19 14:46:01 -0600

    playlist appending to profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -4,7 +4,7 @@ import { Navigate, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 
 
-import Question from "./Question";
+import Playlist from "../components/Playlist";
 
 
 // Material UI
@@ -92,7 +92,7 @@ export default function Profile() {
               QUESTION {profileQuestion}
             </Typography>
             <Typography variant="body2">
-              well meaning and kindly.
+              playlist here <Playlist/>
               <br />
               {'"a benevolent smile"'}
             </Typography>
@@ -105,37 +105,3 @@ export default function Profile() {
     </>
   );
 }
-
-// import * as React from 'react';
-// import Box from '@mui/material/Box';
-// import Card from '@mui/material/Card';
-// import CardActions from '@mui/material/CardActions';
-// import CardContent from '@mui/material/CardContent';
-// import Button from '@mui/material/Button';
-// import Typography from '@mui/material/Typography';
-
-// export default function BasicCard() {
-//   return (
-//     <Card sx={{ minWidth: 275 }}>
-//       <CardContent>
-//         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-//           Word of the Day
-//         </Typography>
-//         <Typography variant="h5" component="div">
-//           benevolent
-//         </Typography>
-//         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-//           adjective
-//         </Typography>
-//         <Typography variant="body2">
-//           well meaning and kindly.
-//           <br />
-//           {'"a benevolent smile"'}
-//         </Typography>
-//       </CardContent>
-//       <CardActions>
-//         <Button size="small">Learn More</Button>
-//       </CardActions>
-//     </Card>
-//   );
-// }
--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,84 +1,97 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const QUERY_USERS = gql`
-    query Users {
-        users {
-            _id
-            username
-            password
-            first_name
-            last_name
-            location
-            age
-            avatar {
-                large
-                medium
-                thumbnail
-
-            }
-            playlist {
-                title
-                artist
-                album
-                year
-            
-            }
-        }
+  query Users {
+    users {
+      _id
+      username
+      password
+      first_name
+      last_name
+      location
+      age
+      avatar {
+        large
+        medium
+        thumbnail
+      }
+      playlist {
+        _id
+        title
+        artist
+        album
+        year
+      }
     }
+  }
 `;
 
 export const QUERY_SINGLE_USER = gql`
-    query getSingleUser($userId: ID!) {
-        user(userId: $userId) {
-            _id
-            username
-            avatar {
-              large
-              medium 
-              thumbnail
-            }
-        }
+  query getSingleUser($userId: ID!) {
+    user(userId: $userId) {
+      _id
+      username
+      avatar {
+        large
+        medium
+        thumbnail
+      }
+      playlist {
+        _id
+        title
+        artist
+        album
+        year
+      }
     }
+  }
 `;
 
 export const QUERY_SONGS = gql`
-    query getSongs {
-        songs {
-            _id
-            title
-            artist
-            album
-            year
-        }
+  query getSongs {
+    songs {
+      _id
+      title
+      artist
+      album
+      year
     }
+  }
 `;
 
 export const QUERY_SINGLE_SONG = gql`
-    query getSingleSong($songId: ID!) {
-        song (songId: $songId){
-            title
-            artist
-            album
-            year
-            genre
-        }
+  query getSingleSong($songId: ID!) {
+    song(songId: $songId) {
+      title
+      artist
+      album
+      year
+      genre
     }
+  }
 `;
 
 export const QUERY_ME = gql`
   query me {
     me {
-    _id
-    username
-    first_name
-    last_name
-    location
-    age
-    avatar {
+      _id
+      username
+      first_name
+      last_name
+      location
+      age
+      avatar {
         large
-        medium 
+        medium
         thumbnail
-        }
+      }
+      playlist {
+        _id
+        title
+        artist
+        album
+        year
+      }
     }
   }
 `;
