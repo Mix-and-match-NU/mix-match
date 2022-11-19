5ababcd7969a3f281d824471f1fa7d7bd1bc001d
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2022-11-19 15:03:55 -0600

    playlist + questions interweave

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,6 +1,8 @@
 import Song from "./Song"
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import { questionData } from "../data/questionData";
+
 
 import { QUERY_ME, QUERY_SINGLE_USER } from "../utils/queries";
 
@@ -17,23 +19,33 @@ function Playlist() {
 
   const destructuredData = data?.me || data?.user || Array(9);
   const songs = destructuredData.playlist
+    console.log(songs)
+  const range = [...Array(9).keys()];
+  const playlist = range.map(
+    index => {
+      const questions = questionData[index]
+      const song = songs[index]
+      console.log(index)
+      console.log('questions',questions)
+      console.log('song',song)
+      return(
+        <>
+          <h2 key={questions.id} question={questions}>{questions.text}</h2>
+          <div key={song._id}>
+            <p>{song.title}</p>
+            <p>{song.artist}</p>
+            <p>{song.album}</p>
+            <p>{song.year}</p> 
+          </div>
+        </>
+      )
+    }
+  )
 
-// console.log('song stuff', data.me.playlist)
-const songList = songs.map(song => {
-  console.log(song, "pls work")
- return (
-  <div key={song._id}>
-  <p>{song.title}</p>
-  {/* <p>{song.artist}</p>
-  <p>{song.album}</p>
-  <p>{song.year}</p> */}
-</div>
- )
-})
 
   return (
     <>
-    <div>{songList}</div>
+    <div>{playlist}</div>
     
     
     </>
