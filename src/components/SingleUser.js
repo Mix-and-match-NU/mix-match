200fabdef55ba0192db06321e9fefd81f64d03a5
Grayson Harvey <graycodes.nu@gmail.com>
2022-11-19 20:55:07 -0600

    home page user cards

--- src/components/SingleUser.js
+++ src/components/SingleUser.js
@@ -1,28 +1,46 @@
 import { avatarClasses } from "@mui/material";
 import { QUERY_USERS } from "../utils/queries";
 
-const SingleUser = ({
-  users,
-}) => {
+// MUI
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import { deepOrange, deepPurple } from "@mui/material/colors";
+
+const SingleUser = ({ users }) => {
   if (!users) {
     return <h3>No Users Yet</h3>;
   }
-  console.log(users)
+  console.log(users);
 
   return (
     <>
-      <div>{users && users.map((user)=> (
-        <div key={user._id}>
-        <h3>{user.username}</h3> 
-        <img alt="headshot" src={user.avatar[0].thumbnail}></img>
-        <p>{user.first_name} {user.last_name}</p>
-        <p>{user.age}</p>
-        <p>{user.location}</p>
-        {/* Needs playlist */}
-        </div>
-      ))}</div>
+      <div>
+        {users &&
+          users.map((user) => (
+            <Card key={user._id}>
+              <CardContent>
+                <Avatar alt="user" 
+                // src={user.avatar[0].thumbnail}
+            sx={{ bgcolor: deepPurple[500] }}
+                ></Avatar>
+                <Typography>
+                  {user.first_name} {user.last_name}
+                </Typography>
+                <Typography>{user.username}</Typography>
+                <Typography>{user.age}</Typography>
+                <Typography>{user.location}</Typography>
+                {/* Needs playlist */}
+              </CardContent>
+            </Card>
+          ))}
+      </div>
     </>
   );
-}
+};
 
 export default SingleUser;
