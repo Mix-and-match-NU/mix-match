c91d39143d19211f57efbd90ad7e4ff2de55cf9d
Grayson Harvey <graycodes.nu@gmail.com>
2022-11-20 21:12:16 -0600

    modal nearly done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -51,151 +51,165 @@ const Navbar = () => {
 
   return (
     <>
-      <AppBar position="static">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              LOGO
-            </Typography>
-
-            <div>
-              {Auth.loggedIn() ? (
-                <>
-                  <Box
-                    sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
-                  >
-                    <Link to="/" style={{ textDecoration: "none" }}>
-                      <Button
-                        onClick={handleCloseNavMenu}
-                        sx={{ my: 2, color: "white", display: "block" }}
-                      >
-                        Home
-                      </Button>
-                    </Link>
-
-                    <Link to="/QuestionList" style={{ textDecoration: "none" }}>
-                      <Button
-                        onClick={handleCloseNavMenu}
-                        sx={{ my: 2, color: "white", display: "block" }}
-                      >
-                        Create A Mix
-                      </Button>
-                    </Link>
-                    <Link to="/Matches" style={{ textDecoration: "none" }}>
-                      <Button
-                        onClick={handleCloseNavMenu}
-                        sx={{ my: 2, color: "white", display: "block" }}
-                      >
-                        My Matches
-                      </Button>
-                    </Link>
-                    <Link to="/Profile" style={{ textDecoration: "none" }}>
-                      <Button
-                        onClick={handleCloseNavMenu}
-                        sx={{ my: 2, color: "white", display: "block" }}
-                      >
-                        Profile
-                      </Button>
-                    </Link>
-                    <Link to="/Login" style={{ textDecoration: "none" }}>
-                      <Button
-                        onClick={logout}
-                        sx={{ my: 2, color: "white", display: "block" }}
-                      >
-                        Logout
-                      </Button>
-                    </Link>
-                  </Box>
-                </>
-              ) : (
-                <>
-                  <Box
-                    sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
-                  >
-                    {" "}
-                    <Link to="/Login" style={{ textDecoration: "none" }}>
-                      <Button
-                        onClick={handleCloseNavMenu}
-                        sx={{ my: 2, color: "white", display: "block" }}
-                      >
-                        Login
-                      </Button>
-                    </Link>
-                    <Link to="/Signup" style={{ textDecoration: "none" }}>
-                      <Button
-                        onClick={handleCloseNavMenu}
-                        sx={{ my: 2, color: "white", display: "block" }}
-                      >
-                        Sign Up
-                      </Button>
-                    </Link>
-                  </Box>
-                </>
-              )}
-            </div>
-          </Toolbar>
-        </Container>
-      </AppBar>
-
       <>
-        <div>
-          <IconButton>
-            <QuestionMarkIcon
-              onClick={handleOpen}
-              color="secondary"
-            ></QuestionMarkIcon>
-          </IconButton>
-          <Modal
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            open={open}
-            onClose={handleClose}
-            closeAfterTransition
-            BackdropComponent={Backdrop}
-            BackdropProps={{
-              timeout: 500,
-            }}
-          >
-            <Fade in={open}>
-              <Box sx={style}>
-                <Typography
-                  id="transition-modal-title"
-                  variant="h6"
-                  component="h2"
-                >
-                  Welcome to Mix-n-Match!
-                </Typography>
-                <Typography
-                  id="transition-modal-description"
-                  sx={{ mt: 2 }}
-                  variant="subtitle1"
-                >
-                  Getting started is easy:{" "}
-                </Typography>
-                <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                  Answer 9 questions related to songs to generate your own
-                  playlist, then view other users' playlists to see who you've
-                  matched with!
-                </Typography>
-              </Box>
-            </Fade>
-          </Modal>
-        </div>
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                LOGO
+              </Typography>
+
+              <div>
+                {Auth.loggedIn() ? (
+                  <>
+                    <Box
+                      sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+                    >
+                      <Link to="/" style={{ textDecoration: "none" }}>
+                        <Button
+                          onClick={handleCloseNavMenu}
+                          sx={{ my: 2, color: "white", display: "block" }}
+                        >
+                          Home
+                        </Button>
+                      </Link>
+
+                      <Link
+                        to="/QuestionList"
+                        style={{ textDecoration: "none" }}
+                      >
+                        <Button
+                          onClick={handleCloseNavMenu}
+                          sx={{ my: 2, color: "white", display: "block" }}
+                        >
+                          Create A Mix
+                        </Button>
+                      </Link>
+                      <Link to="/Matches" style={{ textDecoration: "none" }}>
+                        <Button
+                          onClick={handleCloseNavMenu}
+                          sx={{ my: 2, color: "white", display: "block" }}
+                        >
+                          My Matches
+                        </Button>
+                      </Link>
+                      <Link to="/Profile" style={{ textDecoration: "none" }}>
+                        <Button
+                          onClick={handleCloseNavMenu}
+                          sx={{ my: 2, color: "white", display: "block" }}
+                        >
+                          Profile
+                        </Button>
+                      </Link>
+                      <Link to="/Login" style={{ textDecoration: "none" }}>
+                        <Button
+                          onClick={logout}
+                          sx={{ my: 2, color: "white", display: "block" }}
+                        >
+                          Logout
+                        </Button>
+                      </Link>
+                    </Box>
+                  </>
+                ) : (
+                  <>
+                    <Box
+                      sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+                    >
+                      {" "}
+                      <Link to="/Login" style={{ textDecoration: "none" }}>
+                        <Button
+                          onClick={handleCloseNavMenu}
+                          sx={{ my: 2, color: "white", display: "block" }}
+                        >
+                          Login
+                        </Button>
+                      </Link>
+                      <Link to="/Signup" style={{ textDecoration: "none" }}>
+                        <Button
+                          onClick={handleCloseNavMenu}
+                          sx={{ my: 2, color: "white", display: "block" }}
+                        >
+                          Sign Up
+                        </Button>
+                      </Link>
+                    </Box>
+                  </>
+                )}
+              </div>
+            </Toolbar>
+          </Container>
+        </AppBar>
       </>
+
+      <div>
+        {Auth.loggedIn() ? (
+          <>
+            <div>
+              <IconButton>
+                <QuestionMarkIcon
+                  onClick={handleOpen}
+                  color="secondary"
+                ></QuestionMarkIcon>
+              </IconButton>
+              <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                  timeout: 500,
+                }}
+              >
+                <Fade in={open}>
+                  <Box sx={style}>
+                    <Typography
+                      id="transition-modal-title"
+                      variant="h6"
+                      component="h2"
+                    >
+                      Welcome to Mix-n-Match!
+                    </Typography>
+                    <Typography
+                      id="transition-modal-description"
+                      sx={{ mt: 2 }}
+                      variant="subtitle1"
+                    >
+                      Getting started is easy:{" "}
+                    </Typography>
+                    <Typography
+                      id="transition-modal-description"
+                      sx={{ mt: 2 }}
+                    >
+                      Answer 9 questions related to songs to generate your own
+                      playlist, then view other users' playlists to see who
+                      you've matched with!
+                    </Typography>
+                  </Box>
+                </Fade>
+              </Modal>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
     </>
   );
 };
